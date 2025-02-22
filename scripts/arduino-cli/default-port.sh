981089ad24be0215021cc28d04f0d50c179eb07d
Lorenzo Pistone <blaffablaffa@gmail.com>
2025-02-22 12:33:32 +0100

    drop jq dependency in scripts/arduino-cli/default-port.sh

--- scripts/arduino-cli/default-port.sh
+++ scripts/arduino-cli/default-port.sh
@@ -3,8 +3,7 @@
 set -uo pipefail
 
 if [[ -z "${BLASTIC_SCALE_PORT:-}" ]]; then
-  BLASTIC_SCALE_PORT="$(arduino-cli board list --fqbn arduino:renesas_uno:unor4wifi --json | \
-    jq -r -e '.detected_ports[].port.address, halt_error(if .detected_ports | length != 1 then 10 else 0 end)' 2>/dev/null)"
+  BLASTIC_SCALE_PORT="$(arduino-cli board list --fqbn arduino:renesas_uno:unor4wifi | grep arduino:renesas_uno:unor4wifi | cut -f 1 -d ' ')"
   if [[ $? != 0 ]]; then
     unset BLASTIC_SCALE_PORT
     echo cannot detect a single arduino:renesas_uno:unor4wifi board, define BLASTIC_SCALE_PORT manually 1>&2
